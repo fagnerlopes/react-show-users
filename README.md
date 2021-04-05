069fa5e5a069f97cbe7992d5ceecfffad3f4d425
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-05 14:02:31 -0300

    Toggle and User components

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-show-users
-An Bootacamp IGTI exercise about Class Components using  public API data from https://randomuser.me/api/?seed=rush&amp;nat=br&amp;results=10.
+A Bootacamp IGTI exercise about Class Components using  public API data from https://randomuser.me/api/?seed=rush&amp;nat=br&amp;results=10.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from "react";
+import Toggle from "./components/toggle/Toggle";
+import Users from "./components/users/Users";
 
 export default class App extends Component {
-  
-  render() {
-    return (<div>
+  constructor() {
+    super();
 
-    </div>
+    this.state = {
+      users: [],
+      showUsers: false,
+    };
+  }
+  async componentDidMount() {
+    console.log("componentDidMount de App.js");
+    const res = await fetch(
+      "https://randomuser.me/api/?seed=rush&nat=br&results=10"
+    );
+    const json = await res.json();
+
+    this.setState({
+      users: json.results,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate de App.js");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount de App.js");
+  }
+
+  handleShowUsers = (isChecked) => {
+    this.setState({ showUsers: isChecked });
+  };
+
+  //{ showUsers ? <div>Users</div> : <div>Exibição de usuários desativada</div> }
+
+  render() {
+    const { showUsers, users } = this.state;
+
+    return (
+      <div>
+        <div>
+          <h3>React Lifecycle</h3>
+          <Toggle 
+            description="Mostrar usuários: "
+            enabled={showUsers} 
+            onToggle={this.handleShowUsers}
+          />
+          <hr />
+          {showUsers && <Users users={users} />}
+        </div>
+      </div>
     );
   }
 }
